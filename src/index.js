849d94fc5f0138215517ee311035c367a62c2dc0
Cezar Neaga <hi@cezarneaga.eu>
2020-03-06 14:51:56 +0100

    launching soon v2

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Routes from './config/routes';
-import { browserHistory } from 'react-router';
-import './index.css';
-
-render(<Routes history={browserHistory} />, document.getElementById('root'));
